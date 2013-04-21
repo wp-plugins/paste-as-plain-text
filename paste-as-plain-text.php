3506504316dfdd2aab4a5c2f5f78240a7b1bec5e
tillkruess <tillkruess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-21 12:17:42 +0000

    initial import
    git-svn-id: https://plugins.svn.wordpress.org/paste-as-plain-text/trunk@701087 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ paste-as-plain-text.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Plugin Name: Paste as Plain Text
+Plugin URI: http://wordpress.org/extend/plugins/paste-as-plain-text/
+Description: Forces the WordPress editor to paste everything as plain text.
+Version: 1.0
+Author: Till Krüss
+Author URI: http://tillkruess.com/
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/**
+ * Copyright 2013 Till Krüss  (www.tillkruess.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Paste as Plain Text
+ * @copyright 2013 Till Krüss
+ */
+
+class TK_PasteAsPlainText {
+
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'init' ) );
+	}
+
+	function init() {
+		add_filter( 'tiny_mce_before_init', array($this, 'forcePasteAsPlainText' ) );
+		add_filter( 'teeny_mce_before_init', array($this, 'forcePasteAsPlainText' ) );
+		add_filter( 'teeny_mce_plugins', array($this, 'loadPasteInTeeny' ) );
+		add_filter( 'mce_buttons_2', array($this, 'removePasteAsPlainTextButton' ) );
+	}
+
+	function forcePasteAsPlainText( $mceInit ) {
+		$mceInit[ 'paste_text_sticky' ] = true;
+		$mceInit[ 'paste_text_sticky_default' ] = true;
+		return $mceInit;
+	}
+
+	function loadPasteInTeeny( $plugins ) {
+		$plugins[] = 'paste';
+		return $plugins;
+	}
+
+	function removePasteAsPlainTextButton( $buttons ) {
+		if( ( $key = array_search( 'pastetext', $buttons ) ) !== false ) {
+			unset( $buttons[ $key ] );
+		}
+		return $buttons;
+	}
+
+}
+new TK_PasteAsPlainText();
